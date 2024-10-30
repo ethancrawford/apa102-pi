76dc364bfd092e88fecca180fd951a7c55942f69
Ethan Crawford <ethan_jc@hotmail.com>
2024-10-30 17:45:54 +0800

    Try merging colour picking script into custom theme test

--- runcolorcycle_custom.py
+++ runcolorcycle_custom.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 """Sample script to run a few colour tests on the strip."""
 from apa102_pi.colorschemes import colorschemes
+from coloraide import Color
 
 NUM_LED = 576
 BRIGHTNESS = 15
 
+def _colour_list():
+  num_points = 100
+  interpolation = Color.interpolate(["#dfff0e", "#0eff2f", "#9ac063", "#0ceeb9", "#c2f841"], method="bspline")
+  result = [_convert_colour(interpolation(x / num_points)) for x in range(num_points + 1)]
+  # print([hex(num) for num in result])
+  return result
 
-def main(colour_list=[]):
+def _convert_colour(colour):
+  return int(colour.convert("srgb").to_string(hex=True).removeprefix("#"), 16)
+
+def main():
     # Cycles through a custom colour list
     print('Cycles through a custom colour list')
-
+    colour_list = _colour_list()
     my_cycle = colorschemes.Custom(num_led=NUM_LED, num_steps_per_cycle=len(colour_list), order='rgb', colours=colour_list)
     my_cycle.start()
 
@@ -17,5 +27,4 @@ def main(colour_list=[]):
 
 
 if __name__ == '__main__':
-    import sys
-    main(sys.argv[1])
+    main()
